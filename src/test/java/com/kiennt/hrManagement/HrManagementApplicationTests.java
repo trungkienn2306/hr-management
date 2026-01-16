48e6aa51a9141bda3add1edbcbf25fe6406e252e
kiennt <trungkienn2306@gmail.com>
2026-01-16 14:25:24 +0700

    initial commit

---
+++ src/test/java/com/kiennt/hrManagement/HrManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiennt.hrManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
